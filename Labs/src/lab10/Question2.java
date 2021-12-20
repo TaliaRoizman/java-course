9a5246aad48fc1436ab8442392d9c4ad105042cc
Talia <Talia@DESKTOP-QRKE2ES>
2021-12-20 08:42:01 +0200

    update

--- Labs/src/lab10/Question2.java
+++ Labs/src/lab10/Question2.java
@@ -4,16 +4,24 @@ public class Question2 {
 
 	public static void main(String[] args) {
 		int[] arr1 = { 1, 3, 4, 4, 8, 2, 6, 8, 1, 9, 0, 5, 6, 7, 5 };
-		int[] arr2 = new int[arr1.length];
-		for (int i = 0; i < arr1.length; i++) {
-			int c = 0;
-			for (int j = 1; j < 10; j++) {
-				if(arr1[i] == j) {
-					c++;
+		int places = 0, max = 0;
+		while (places <= 9) {
+			for (int i = 0; i < arr1.length; i++) {
+				int c = 0;
+				for (int j = 0; j < 10; j++) {
+					if (arr1[i] == j) {
+						c++;
+					}
+				}
+				if (c > 0) {
+					places++;
 				}
 			}
-			if (c > 0) {
-				
+		}
+		int[] arr2 = new int[places];
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < arr1.length; j++) {
+				if (arr[j] == i)
 			}
 		}
 	}
