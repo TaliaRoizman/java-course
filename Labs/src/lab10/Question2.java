6d06036f27d2d493f3881fee0304c722467494a5
Talia <Talia@DESKTOP-QRKE2ES>
2021-12-22 23:36:44 +0200

    update

--- Labs/src/lab10/Question2.java
+++ Labs/src/lab10/Question2.java
@@ -1,33 +1,48 @@
 package lab10;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class Question2 {
 
 	public static void main(String[] args) {
-		int[] arr1 = { 1, 3, 8, 2, 6, 8, 1, 9, 0, 5, 6, 5 };
-		int c = 0;
-		boolean isVal = false;
-		for (int j = 0; j < 10; j++) {
-			for (int i = 0; i < arr1.length; i++) {
-				if (arr1[i] == j) {
-					isVal = true;
-					break;
-				}
-			}
-			if (isVal == true) {
-				c++;
-			}
-		}
-		System.out.println(c);
-		int[] arr2 = new int[c];
-		for (int i = 0; i <= c; i++) {
-			for (int j = 0; j < arr1.length; j++) {
-				if (arr1[j] == i) {
-					arr2[i] = i;
+		int c = 0, size = 0;
+		boolean isZero = false;
+		int[] arr = { 1, 3, 0, 5, 7, 7, 3, 5, 6, 0, 2 };
+		int[] copy = new int[arr.length];
+		System.out.println(Arrays.toString(arr));
+
+		for (int i = 0; i < arr.length; i++) {
+			c = arr[i];
+			for (int j = (i + 1); j < arr.length; j++) {
+				if (arr[j] == c) {
+					arr[j] = 0;
 				}
 			}
 		}
-		System.out.println(Arrays.toString(arr2));
+		System.out.println(Arrays.toString(arr));
+		for (int i = 0; i < copy.length; i++) {
+			if (arr[i] != 0) {
+				size++;
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				isZero = true;
+			}
+		}
+		if (isZero == true) {
+			size++;
+		}
+		int[] newArr = new int[size];
+		int i=0;
+		for (int j = 0; j < arr.length; j++) {
+			while(arr[j]!=0 && i<=5) {
+				newArr[i] = arr[j];
+				i++;
+				break;
+			}
+		}		
+		System.out.println(Arrays.toString(newArr));
 	}
 }
